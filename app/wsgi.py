17302ba7cc73ffa35f2588686286e0e1f245d67b
ChandanaBG10 <chandanac641@gmail.com>
2026-05-27 21:09:05 +0530

    Added

--- app/wsgi.py
+++ app/wsgi.py
@@ -1,12 +1,3 @@
-"""
-WSGI config for app project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/6.0/howto/deployment/wsgi/
-"""
-
 import os
 
 from django.core.wsgi import get_wsgi_application
@@ -14,3 +5,12 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'app.settings')
 
 application = get_wsgi_application()
+
+
+# RUN MIGRATIONS AUTOMATICALLY
+from django.core.management import call_command
+
+try:
+    call_command('migrate', interactive=False)
+except Exception as e:
+    print(e)
